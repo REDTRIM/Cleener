c6951bba2161f3112cddcff820b1234e9682c8aa
REDTRIM <redtrim.pro@gmail.com>
2025-04-03 20:31:18 +0200

    Update Lqnnix #3.bat

--- Lqnnix #3.bat
+++ Lqnnix #3.bat
@@ -7549,4 +7549,3 @@ del C:\Users\%username%\AppData\Local\FortniteGame\Saved\PersistentDownloadDir\S
 del C:\Users\%username%\AppData\Local\FortniteGame\Saved\PersistentDownloadDir\StagingBundles\Lang.zh-CNOptional /f /s /q
 del C:\Users\%username%\AppData\Local\FortniteGame\Saved\PersistentDownloadDir\StagingBundles\StartupOptional /f /s /q
 pause
-exit
